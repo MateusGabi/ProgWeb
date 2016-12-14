08c35ad969afe82859115184c54345715fea665f
MATEUS GABI/UFMS <mateus.moreira@aluno.ufms.br>
2016-12-14 16:46:00 -0300

    inserido layout

---
+++ ex6/View/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <!-- Bootstrap --><!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body class="container">
+        <div class="page-header">
+            <h1>Agenda <small>Exercício 6 Prog Web</small></h1>
+        </div>
+
+        <div class="btn-group btn-group-justified" role="group">
+            <a class="btn btn-primary active" href="../View/index.php">Início</a>
+            <a class="btn btn-default" href="../Controller/controle.php?op=novo">Criar contato</a>
+            <a class="btn btn-default" href="../Controller/controle.php?op=lista">Listar contatos</a>
+            <a class="btn btn-default" href="../Controller/controle.php?op=sair">Sair</a>
+        </div>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    </body>
+</html>
--- ex6/index.php
+++ ex6/index.php
@@ -1,20 +1,20 @@
-<?php
-
-if (!isset($_GET['pagina'])) {
-    include_once(__DIR__ . "/view/index.php");
-} else {
-
-    $page = "error.php";
-
-    switch ($_GET['pagina']) {
-        case "conta/novo":
-            $page = "novo.php";
-            break;
-        case "contato/lista":
-            $page = "lista.php";
-            break;
-    }
-
-    include_once(__DIR__ . "/view/" . $page);
-}
-
+<!--
+ * Material utilizado para as aulas práticas da disciplinas da Faculdade de
+ * Computação da Universidade Federal de Mato Grosso do Sul (FACOM / UFMS).
+ * Seu uso é permitido para fins apenas acadêmicos, todavia mantendo a
+ * referência de autoria.
+ * @author Profª Jane Eleutério
+-->
+<!DOCTYPE html>
+<!--
+Essa página apenas redireciona a URL para View/index.php
+-->
+<html>
+    <head>
+        <title>Redirecionando....</title>
+        <meta http-equiv="refresh" content="0; url=View/index.php">
+    </head>
+    <body>
+        <!-- Não possui conteúdo -->
+    </body>
+</html>
